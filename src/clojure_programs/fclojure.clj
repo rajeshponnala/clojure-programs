5e7a74ac8cc4c1797a9a13aeca53d2b5f1da4bff
Your Full Name <Your EMail Id>
2015-09-08 02:08:30 +0530

    implement gcd,symm-diff

--- src/clojure_programs/fclojure.clj
+++ src/clojure_programs/fclojure.clj
@@ -1,7 +1,8 @@
 (ns clojure.4clojure
   (:gen-class)
   (:use [clojure.string :only [join split]])
-  (:use [clojure.core :only [filter]]))
+  (:use [clojure.core :only [filter]])
+  (:use [clojure.set :only [union difference]]))
 
 (defn big-divide [n p1 p2]
   (reduce (fn [x y] (cond (or (zero? (mod y p1))  (zero? (mod y p2))) (+ x y)
@@ -12,3 +13,10 @@
                               (cond (= n cnt) false
                                     (= n (* cnt cnt)) true
                                     :else (recur n (inc cnt))))) (split s #","))))
+
+(defn gcd [a b]
+  (if (zero? b) a
+      (recur b (mod a b))))
+
+(defn symm-diff [s1 s2]
+  (union (difference s1 s2) (difference s2 s1)))
